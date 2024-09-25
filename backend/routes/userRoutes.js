17777d546a318f7a139fe400851e9aaf6dec576e
unknown <daffatabianoo@gmail.com>
2024-09-25 10:35:37 +0700

    add(backend ): preparations packages server

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controller/userController.js';
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from '../controller/productController.js';
+
+const router = express.Router();
+
+router.get('/users', getUsers);
+router.get('/user/:id', getUserById);
+router.post('/users', createUser);
+router.patch('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
+
+router.get('/products', getProducts);
+router.get('/product/:id', getProductById);
+router.post('/products', createProduct);
+router.patch('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
+
+export default router;
