370dda5effcf860cbd7da839862c1c64291bf5ea
unknown <daffatabianoo@gmail.com>
2024-09-25 10:55:09 +0700

    feat(backend): login backend features service

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
 
 export const getUsers = async (req, res) => {
   try {
@@ -21,6 +22,32 @@ export const getUserById = async (req, res) => {
     console.log(error.message);
   }
 };
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 export const createUser = async (req, res) => {
   try {
     await User.create(req.body);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   createUser,
   updateUser,
   deleteUser,
+  Login,
 } from '../controller/userController.js';
 import {
   createProduct,
@@ -21,6 +22,7 @@ router.get('/user/:id', getUserById);
 router.post('/users', createUser);
 router.patch('/user/:id', updateUser);
 router.delete('/user/:id', deleteUser);
+router.get('/login', Login);
 
 router.get('/products', getProducts);
 router.get('/product/:id', getProductById);
